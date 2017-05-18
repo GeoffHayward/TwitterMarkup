af5e9030043ab8a8efff2b12a512a379a73d1010
Geoff Hayward <git@geoffhayward.eu>
2017-05-18 17:21:37 +0100

    Refactors to use object closure to hide internals

--- TwitterMarkup.js
+++ TwitterMarkup.js
@@ -3,51 +3,43 @@
 *   TwitterMarkup
 *   
 * Description 
-*   You can use this object to markup raw Twitter tweets urls, usernames and hashtags.
+*   You can use this object to markup DOM elements that hold raw Twitter tweets urls, usernames and hashtags.
 * 
 * TwitterMarkup by Geoff Hayward - http://geoffhayward.eu
 */ 
-var TwitterMarkup = {
+var TwitterMarkup = (function(){
 	
     /*
-    * Consider method as public 
-    * 
-    * This performs the markup transform on a DOM element that contains raw Twitter
-    * tweets.
+    * Performs the Twiter markup transform on a given DOM element, and the element's sub-elements.
     */
-    transform: function(element){
+    function transform(element){
         var ts = element.innerHTML;
-        ts = TwitterMarkup._parseURLs(ts);
-        ts = TwitterMarkup._parseUsernames(ts);
-        ts = TwitterMarkup._parseHashtags(ts);
+        ts = _parseURLs(ts);
+        ts = _parseUsernames(ts);
+        ts = _parseHashtags(ts);
         element.innerHTML = ts;
-    },
-    
-    /*
-    * Consider method as private.
-    */      
-    _parseURLs: function(tweetsString) {
+    };
+         
+    function _parseURLs(tweetsString) {
         return tweetsString.replace(/(https?:\/\/t.co\/[A-Z0-9-_]{10})/ig, function(match, group1) {
             return '<a href="' + group1 + '" target="_blank">' + match + '</a>';
         });
-    },
-    
-    /*
-    * Consider method as private.
-    */      
-    _parseUsernames: function(tweetsString){
+    };
+         
+    function _parseUsernames(tweetsString){
         return tweetsString.replace(/[@]+([A-Z0-9_]+)/ig, function(match, group1) {
             return '<a href="http://twitter.com/' + group1 + '" target="_blank">' + match + '</a>'; 
-	});
-    },
-    
-    /*
-    * Consider method as private.
-    */      
-    _parseHashtags: function(tweetsString){
+		});
+    };
+          
+    function _parseHashtags(tweetsString){
         return tweetsString.replace(/[#]+([A-Za-z0-9-_]+)/ig, function(match, group1) {
             return '<a href="http://twitter.com/search?q=%23' + group1 + '" target="_blank">' + match + '</a>'; 
         });
-    }
+    };
+	
+	return {
+		transform : transform
+	};
     
-};
+})();
